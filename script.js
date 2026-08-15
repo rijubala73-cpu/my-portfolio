f1f2fb3c86057d5e4c840dbd5d7f8a166b8faadb
Riju Bala <rijubala73@gmail.com>
2026-08-15 21:16:04 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Toggle Navbar Menu on Mobile
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+// Scroll Active Link Highlight
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Remove Toggle Icon and Navbar on Scroll Link Click
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
